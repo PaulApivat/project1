5ecee3a01b2a1347c70495da34815fb262accc5d
Paul Apivat <paul.apivat@gmail.com>
2016-03-13 22:25:15 +0700

    Add Slide Panel JQuery

--- project1.js
+++ project1.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 	$('.box').click(function(){
-		$(this).fadeOut('slow');
+		$(this).fadeOut('slow');      
+	});
+});
+
+$(document).ready(function(){
+	$('.box1').mouseenter(function(){
+		$(this).fadeOut('slow');      
+	});
+});
+
+
+$(document).ready(function(){
+	$('.pull-me').click(function(){
+		$('.panel').slideToggle('slow');
 	});
 });

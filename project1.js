d1d6bff2c419c8c366b976dadffd544dbcb09e0a
Paul Apivat <paul.apivat@gmail.com>
2016-03-17 14:32:56 +0700

    jQuery addition
    Change HTML elements

--- project1.js
+++ project1.js
@@ -22,3 +22,15 @@ $(document).ready(function(){
 		$(this).toggleClass('highlighted');
 	});
 });
+
+$(document).ready(function(){
+	$('.magic').html("Click function in action (JQuery). This sentence is being added by jQuery magic, you won't find it in the html file.")
+});
+
+$(document).ready(function(){
+	$('.magic1').html("MouseEnter function in action (JQuery), with additional jQuery magic added in this sentence. Check html file.")
+});
+
+$(document).ready(function(){
+	$('.magic2').html("Toggle Highlights and [JQUERY MAGIC HERE!]");
+});
